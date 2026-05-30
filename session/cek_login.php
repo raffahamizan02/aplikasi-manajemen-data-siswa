e399463fe21fcae65980ace76a4c0c4f55ab4627
Muhammad Abhiraffa Hamizan <raplhy02@gmail.com>
2026-05-30 12:27:38 +0700

    cek login dan logout

---
+++ session/cek_login.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+// panggil koneksi
+include 'Koneksi.php';
+
+// ambil data dari form
+$username = $_POST['user'];
+$password = $_POST['pass'];
+
+// enkripsi password dengan md5
+$password_md5 = md5($password);
+
+// query cek login
+$query = mysqli_query($koneksi, "SELECT * FROM pengguna WHERE username='$username' AND password='$password_md5'");
+$cek = mysqli_num_rows($query);
+
+if ($cek > 0) {
+    $_SESSION['login'] = true;
+    $_SESSION['user'] = $username;
+    header("location: home.php");
+} else {
+    header("location: index.php?p=Username atau Password salah!");
+}
+?>
